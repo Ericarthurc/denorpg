f84446f3e04f2ba0497057d46f38ecc869de6b21
Eric Christensen <hebrew.8.12@gmail.com>
2021-11-18 21:49:29 -0800

    init commit

---
+++ main.ts
@@ -0,0 +1,38 @@
+import { roles, races, Roles, Races } from "./stats.ts";
+
+function input(message = ""): string {
+  const buf = new Uint8Array(1024);
+  Deno.stdout.writeSync(new TextEncoder().encode(`${message} `));
+  const n = <number>Deno.stdin.readSync(buf);
+  return new TextDecoder().decode(buf.subarray(0, n)).trim();
+}
+
+async function asyncInput(message = ""): Promise<string> {
+  const buf = new Uint8Array(1024);
+  await Deno.stdout.write(new TextEncoder().encode(`${message} `));
+  const n = <number>await Deno.stdin.read(buf);
+  return new TextDecoder().decode(buf.subarray(0, n)).trim();
+}
+
+class Player {
+  constructor(_name: string, _race: Races, _role: Roles) {
+    this.name = _name;
+    this.race = _race;
+    this.role = _role;
+
+    this.health = roles[_role].health + races[_race].health;
+    this.attack = roles[_role].attack + races[_race].attack;
+  }
+
+  public name;
+  public race;
+  public role;
+  private health;
+  private attack;
+
+  public slash(p: Player) {}
+}
+
+const PlayerOne = new Player("Eric", "elf", "warrior");
+
+console.log(PlayerOne);
